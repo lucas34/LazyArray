842e47672af7e822a7e60493ec2bb383662a3586
Lucas Nelaupe <lucas34@users.noreply.github.com>
2017-11-23 15:32:29 +0800

    Add language in target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "LazyArray",
-            targets: ["LazyArray"]),
+            targets: ["LazyArray"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,6 +23,7 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "LazyArrayTests",
-            dependencies: ["LazyArray"]),
-    ]
+            dependencies: ["LazyArray"])
+    ],
+    swiftLanguageVersions: [3, 4]
 )
